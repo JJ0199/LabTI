a9b3219cde5a006b9a7f2571b289c28e8666c8cc
IsraelReyesGarcia <li_israelreyes@unca.edu.mx>
2022-09-14 17:26:30 -0500

    Agregué carpetas para la estructura mvc

---
+++ modelo/Conexion.php
@@ -0,0 +1,17 @@
+<?php 
+    class Conexion{
+        private $sql;
+
+        public function __construct(){
+            $this->sql = new mysqli('localhost', 'root','','labti');
+            if($this->sql->errno){
+                echo "<script>console.log('Error en la conexión'); </script>"; 
+            }
+        }
+
+        
+
+
+    }
+
+?>
