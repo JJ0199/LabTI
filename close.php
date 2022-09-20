1a6be96e20c2fe731b207451c955a9503f2e8a8c
romulos <josue9901@outlook.es>
2022-09-20 16:50:11 -0500

    Nuevo orden

--- close.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if ($_SESSION['nombre']) {
-    session_destroy();
-    header('Location: http://localhost/mat/');
-} else {
-    header('Location: http://localhost/mat/');
-}
