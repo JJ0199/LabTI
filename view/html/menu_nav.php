5552177873d2de26dde4cd760545d3b067852c3f
romulos <josue9901@outlook.es>
2022-09-20 19:34:34 -0500

    Menu

---
+++ view/html/menu_nav.php
@@ -0,0 +1,22 @@
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32">
+            <use xlink:href="#bootstrap"></use>
+        </svg>
+        <span class="fs-4">LaBTIC</span>
+    </a>
+
+    <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Áreas</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Miembros</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Investigación</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Difusión</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Acerca de</a></li>
+    </ul>
+
+    <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-2">Login</button>
+        <!-- <button type="button" class="btn btn-primary">Sign-up</button> -->
+      </div>
+</header>
