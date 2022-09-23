f44a185302327e2827513d30f53293a92149fa33
romulos <josue9901@outlook.es>
2022-09-23 14:58:14 -0500

    vistas

---
+++ view/html/Acerca_de.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acerca de</title>
+
+    <!-- Bootstrap -->
+    <?php include("bootstrap_v5.php"); ?>
+
+    <!-- Para el menu -->
+    <!-- <script src="view/js/desplegar_menu.js"></script> -->
+
+    <!--Iconos-->
+    <link rel="shortcut icon" href="..//img/icono_lic_info.png">
+
+</head>
+<body>
+    <div class="container">
+        <?php include("menu_nav.php"); ?>
+    </div>
+
+    <main>
+        
+            <!-- Espacio -->
+        <div class="container">
+            <div class="mt-5"></div>
+            <div class="mt-5"></div>
+            <div class="mt-5"></div>
+        </div>
+
+        <div class="container">
+            <H1>sjcnisc</H1>
+            <?php echo $link ?>
+            <p>Maro Muchi, artista del manga de comedia Seitokai ni mo Ana wa Aru! (There Is Also a Hole in the Student Organization!), compartió una ilustración erótica en Twitter que se volvió inmediatamente viral, aunque por las razones equivocadas. Resulta que su ilustración atrajo la atención del colectivo feminista en redes sociales, quienes inmediatamente comenzaron a atacarlo.
+
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto.</p>
+        </div>
+
+        <div id="id_mision" class="container">
+            <a>Mision</a>
+            <p>Maro Muchi, artista del manga de comedia Seitokai ni mo Ana wa Aru! (There Is Also a Hole in the Student Organization!), compartió una ilustración erótica en Twitter que se volvió inmediatamente viral, aunque por las razones equivocadas. Resulta que su ilustración atrajo la atención del colectivo feminista en redes sociales, quienes inmediatamente comenzaron a atacarlo.
+
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto
+            Maro Muchi, artista del manga de comedia Seitokai ni mo Ana wa Aru! (There Is Also a Hole in the Student Organization!), compartió una ilustración erótica en Twitter que se volvió inmediatamente viral, aunque por las razones equivocadas. Resulta que su ilustración atrajo la atención del colectivo feminista en redes sociales, quienes inmediatamente comenzaron a atacarlo.
+
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto.
+            Maro Muchi, artista del manga de comedia Seitokai ni mo Ana wa Aru! (There Is Also a Hole in the Student Organization!), compartió una ilustración erótica en Twitter que se volvió inmediatamente viral, aunque por las razones equivocadas. Resulta que su ilustración atrajo la atención del colectivo feminista en redes sociales, quienes inmediatamente comenzaron a atacarlo.
+
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto.
+            Maro Muchi, artista del manga de comedia Seitokai ni mo Ana wa Aru! (There Is Also a Hole in the Student Organization!), compartió una ilustración erótica en Twitter que se volvió inmediatamente viral, aunque por las razones equivocadas. Resulta que su ilustración atrajo la atención del colectivo feminista en redes sociales, quienes inmediatamente comenzaron a atacarlo.
+
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto..</p>
+        </div>
+
+        <!-- Espacio -->
+        <div class="container">
+            <div class="mt-5"></div>
+            <div class="mt-5"></div>
+            <div class="mt-5"></div>
+        </div>
+
+        <div id="id_vision" class="container">
+            <a>Vision</a>
+            <p>Maro Muchi, artista del manga de comedia Seitokai ni mo Ana wa Aru! (There Is Also a Hole in the Student Organization!), compartió una ilustración erótica en Twitter que se volvió inmediatamente viral, aunque por las razones equivocadas. Resulta que su ilustración atrajo la atención del colectivo feminista en redes sociales, quienes inmediatamente comenzaron a atacarlo.
+
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto.
+            Maro Muchi, artista del manga de comedia Seitokai ni mo Ana wa Aru! (There Is Also a Hole in the Student Organization!), compartió una ilustración erótica en Twitter que se volvió inmediatamente viral, aunque por las razones equivocadas. Resulta que su ilustración atrajo la atención del colectivo feminista en redes sociales, quienes inmediatamente comenzaron a atacarlo.
+
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto.
+            Maro Muchi, artista del manga de comedia Seitokai ni mo Ana wa Aru! (There Is Also a Hole in the Student Organization!), compartió una ilustración erótica en Twitter que se volvió inmediatamente viral, aunque por las razones equivocadas. Resulta que su ilustración atrajo la atención del colectivo feminista en redes sociales, quienes inmediatamente comenzaron a atacarlo.
+
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto.
+            Maro Muchi, artista del manga de comedia Seitokai ni mo Ana wa Aru! (There Is Also a Hole in the Student Organization!), compartió una ilustración erótica en Twitter que se volvió inmediatamente viral, aunque por las razones equivocadas. Resulta que su ilustración atrajo la atención del colectivo feminista en redes sociales, quienes inmediatamente comenzaron a atacarlo.
+
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto.</p>
+
+        </div>
+
+        <!-- Espacio -->
+        <div class="container">
+            <div class="mt-5"></div>
+            <hr>
+            <div class="mt-5"></div>
+        </div>
+
+
+        <!-- Desarrolladores -->
+        <div id="des" class="container">
+
+            <h1>Desarrolladores</h1>
+            <br>
+
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+
+                <!-- Miembro -->
+                <div class="col">
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="..//img/maria_peralta_calvo.jpg" class="card-img-top" alt="...">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Miembro -->
+                <div class="col">
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="..//img/icono_lic_info.png" class="card-img-top" alt="...">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+
+        <!-- Espacio -->
+        <div class="container">
+            <div class="mt-5"></div>
+            <div class="mt-5"></div>
+            <div class="mt-5"></div>
+        </div>
+
+        <div id="id_contactenos" class="container">
+            <a>Contáctenos</a>
+            <p>
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto.
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto.
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto.
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto.
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto.
+            El autor escribió: «La profesora de Ume-kun que hurga en la nevera a altas horas de la noche en busca de comida que no haya caducado», pero cabe mencionar que, de acuerdo con su descripción oficial en el manga, es una persona que va a casa, come y se masturba frecuentemente. Por otra parte, en la ilustración se puede ver a la profesora hurgando en la nevera con el sostén abierto.
+
+            </p>
+        </div>
+
+        <!-- Espacio -->
+        <div class="container">
+            <div class="mt-5"></div>
+            <div class="mt-5"></div>
+            <div class="mt-5"></div>
+        </div>
+        
+        <!-- Mapa -->
+        <div id="lab_ubic" class="container">
+            <h1>Ubicación</h1>
+            <div class="ratio ratio-16x9">
+                <iframe src="https://www.google.com/maps/embed?pb=!4v1663875153084!6m8!1m7!1sw5TiNt1fY9mSNBinB1_XEA!2m2!1d18.13510447379684!2d-97.09705874914933!3f71.45409423395809!4f-26.282944461073562!5f1.1924812503605782" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </main>
+
+    <?php include("footer.php"); ?>
+</body>
+</html>
---
+++ view/html/menu_nav.php
@@ -0,0 +1,163 @@
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom ">
+    <?php 
+        $link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+        if($link == "http://www.labtiunca.edu.mx/"){
+            $link_img = "view/img/icono_lic_info_v5.png";
+        }
+        if($link != "http://www.labtiunca.edu.mx/"){
+            $link_img = "..//img/icono_lic_info_v5.png";
+        }
+    ?>
+    
+    <div class="col align-self-center">
+        <a href="http://www.labti.unca.edu.mx" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <img src=<?php echo $link_img;?> alt="" width="300" height="120" class="d-inline-block align-text-top">
+            <!-- <span class="fs-4">  LabTic </span> -->
+        </a>
+    </div>
+
+    <div class="col align-items-end">
+      <!--   <a>LabTic</a> -->
+    </div>
+
+    <div class="col col-md-2"></div>
+
+    <div class="col align-self-end">
+        <nav class="nav nav-pills navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent items_en_uso">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                        <!-- Home -->
+                        <li class="nav-item">
+                            <a class="nav-link " aria-current="page" href="http://www.labtiunca.edu.mx">Inicio</a>
+                        </li>
+
+                        <!-- áreas del laboratorio - lista desplegable -->
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false" role="button">Áreas</a>
+
+                            <!-- Lista de las areas del laboratorio -->
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown" id="dmenu">
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Sistemas_inteligentes_y_distribuidos.php">Sistemas inteligentes y distribuidos</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Electronica.php">Electronica</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Investigación_y_desarrollo_de_software.php">Investigación y desarrollo de software</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Experiencia_de_usuario_UX.php">Experiencia de usuario UX</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Redes.php">Redes</a>
+                                </li>
+                            </ul>
+
+                        </li>
+
+                        <!-- Miembros del laboratorio - lista desplegable -->
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false" role="button">Miembros</a>
+
+                            <!-- Lista de miembros -->
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Profesores_investigadores.php">Profesores investigadores</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Colaboradores.php">Colaboradores</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Servicio_Social.php">Servicio Social</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Estancias_profesionales.php">Estancias profesionales</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Alumni_Egresados.php">Alumni-Egresados</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <!-- Investigación - lista desplegable -->
+                        <li class="nav-item">
+                            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false" role="button">Investigación</a>
+
+                            <!-- Lista investigación -->
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Proyectos.php">Proyectos</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Cuerpos_académicos.php">Cuerpos académicos</a>
+                                </li>
+                            </ul>
+                            
+                        </li>
+
+                        <!-- Difusión - lista desplegable -->
+                        <li class="nav-item">
+                            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false" role="button"> Difusión</a>
+
+                            <!-- Lista difusión -->
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Historia.php">Historia</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Noticias.php">Noticias</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Eventos.php">Eventos</a>
+                                </li>
+
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Multimedia.php">Multimedia</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <!-- Acerca de - lista desplegable  solo una pagina -->
+                        <li class="nav-item">
+                            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false" role="button">Acerca de</a>
+
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Acerca_de.php#id_mision">Misión</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Acerca_de.php#id_vision">Visión</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="/view/html/Acerca_de.php#id_contactenos">Contáctenos</a>
+                                </li>
+                            </ul>
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <div class="row align-items-end">
+        <div class="col-md-3 text-end align-self-start">
+            <button type="button" class="btn btn-outline-primary me-2">Login</button>
+            <!-- <button type="button" class="btn btn-primary">Sign-up</button> -->
+        </div>
+    </div>
+</header>
